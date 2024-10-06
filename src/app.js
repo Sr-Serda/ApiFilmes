dafa2d881cba3bd916096b817e8f77a5e45c3530
Gustavo <moreirasilvagustavo13@gmail.com>
2024-10-06 17:45:27 -0300

    fix: db connection

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose"; // Importe mongoose para acessar mongoose.connection
 import connectDatabase from "./config/dbConnection.js";
 import routes from "./routes/index.js";
 
@@ -9,16 +10,18 @@ const app = express();
 app.use(cors());
 
 // Conectar ao banco de dados
-const conexao = await connectDatabase();
+await connectDatabase(); // Chame a função de conexão, mas não armazene o retorno
 
-conexao.on("error", (erro) => {
+// Use mongoose.connection para eventos
+mongoose.connection.on("error", (erro) => {
     console.error("Erro de conexão", erro);
 });
 
-conexao.once("open", () => {
-    console.log("Conexão Feita");
+mongoose.connection.once("open", () => {
+    console.log("Conexão com o MongoDB estabelecida com sucesso.");
 });
 
+// Rotas
 routes(app);
 
 export default app;
