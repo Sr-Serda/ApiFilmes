253267795679a145c6bf78b057499175309c077a
Gustavo <moreirasilvagustavo13@gmail.com>
2024-10-06 04:07:58 -0300

    fix: arrumando timeout do bd

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -4,9 +4,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function connectDatabase() {
-    mongoose.connect(process.env.DB_CONNECTION_STRING);
-    //Conexão com o mongoDB
-    return mongoose.connection;
+    try {
+        await mongoose.connect(process.env.DB_CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000, // timeout de 5 segundos para conexão com o banco de dados
+        });
+        console.log("Conexão com MongoDB estabelecida com sucesso.");
+        return mongoose.connection;
+    } catch (error) {
+        console.error("Erro ao conectar com o MongoDB:", error);
+        throw error;
+    }
 }
+
 
 export default connectDatabase
