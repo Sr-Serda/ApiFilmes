9a44eb6cb4fd4dd1d5f7cd1a2ada67348211d680
Gustavo <moreirasilvagustavo13@gmail.com>
2024-10-06 03:41:07 -0300

    fix: Arrumando variável de ambiente

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import app from "./src/app.js"
-const PORT = 3000
+const PORT = process.env.PORT || 3007 
 
   app.listen(PORT, () => {
     console.log("servidor escutando!");
