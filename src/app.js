ad39c7de48770eba24ba0f5227ff3033516d95e8
Gustavo <moreirasilvagustavo13@gmail.com>
2024-10-16 14:59:23 -0300

    fix: Arrumando CORS 2.4

--- src/app.js
+++ src/app.js
@@ -7,34 +7,34 @@ import routes from "./routes/index.js";
 const app = express();
 
 // Ativar CORS
-var allowedOrigins = ['http://localhost:3000',
-                      'https://6703f2c540ac100008bd980b--serdanoirflix.netlify.app/',
-                        "*"];
+const allowedOrigins = ['http://localhost:3000', 'https://serdanoirflix.netlify.app'];
+
 app.use(cors({
   origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
+    // Permitir solicitações sem origem (como mobile apps ou curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
-  }
+  },
+  // Adicionar suporte para cabeçalhos adicionais, se necessário
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  credentials: true
 }));
 
-
 // Conectar ao banco de dados
-await connectDatabase(); // Aguarda a conexão com o banco de dados, mas não usa o retorno
+await connectDatabase(); // Aguarda a conexão com o banco de dados
 
 // Verificar erros e abrir conexão diretamente pelo mongoose.connection
 mongoose.connection.on("error", (erro) => {
-    console.error("Erro de conexão com o MongoDB:", erro);
+  console.error("Erro de conexão com o MongoDB:", erro);
 });
 
 mongoose.connection.once("open", () => {
-    console.log("Conexão com MongoDB estabelecida com sucesso.");
+  console.log("Conexão com MongoDB estabelecida com sucesso.");
 });
 
 // Rotas
